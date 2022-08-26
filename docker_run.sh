cfc0c0cf5eb9c3fb708d945aef0d9d9b3f4fa03a
p_pjmzhu <zhujunming@tenops.com>
2022-08-27 03:16:53 +0800

    docker-compose done

--- docker_run.sh
+++ docker_run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+#进行测试
 gunicorn start:app -c gunicorn.conf.py
 #while true ; do
 ##    sleep 43200   0.5day
@@ -7,6 +7,14 @@ gunicorn start:app -c gunicorn.conf.py
 #    sleep 2
 #
 #done
-# docker build  --network=host -t testfnd .
+# docker build  --network=host -t testfund:v1 .
 
-0 1 * * * docker exec stock2 /bin/sh -c 'wget localhost:8090/new > /dev/null 2>&1 &'
+docker build  --network=host -t testfund:v1 .
+#docker run -itd --network=host --name=fund testfund:v1 bash
+# 加root可以访问，同时setenforce 0
+#docker run -d --network host --name fund  --privileged=true -v /tmp/fund/fund/fund.sqlite3:/Project/demo/fund/fund/fund.sqlite3 testfnd
+docker run -d --network=host --name=fund testfund:v1
+
+# 设置定时任务触发启动爬虫入库
+0 1 * * *  docker exec stock2 /bin/sh -c 'wget localhost:8090/new > /dev/null 2>&1 &'
+0 1 * * *  curl localhost:8090/new > /tmp/fund.txt 2>&1 &
